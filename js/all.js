bbec5127f09111249203bd1f5c5cf220fe48d18b
“PeggyTsai” <pinklover1671291991@gmail.com>
2021-12-09 21:40:50 +0800

    顯示產品列表

--- js/all.js
+++ js/all.js
@@ -1,9 +1,17 @@
+
+//選取在顯示產品列表的ul標籤
+const productList = document.querySelector(".js-productList");
+
+
+let productData = [];
 //取得產品列表資料
 function getProductList (){
     axios.get(`https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/products`)
     .then(function (response) {
         // handle success
-        console.log(response);
+          productData = response.data.products;
+          console.log(productData);
+          renderProductList(productData);
       })
       .catch(function (error) {
         // handle error
@@ -11,3 +19,19 @@ function getProductList (){
       })
 }
 getProductList();
+
+//將產品列表渲染到網頁上
+function renderProductList(productData){
+  let str ="";
+  productData.forEach(function(item){
+    str+= `<li class="productCard">
+    <h4 class="productType">新品</h4>
+    <img src="${item.images}" alt="${item.description}">
+    <a href="#" class="addCardBtn">加入購物車</a>
+    <h3>${item.title}</h3>
+    <del class="originPrice">NT$ ${item.origin_price}</del>
+    <p class="nowPrice">NT$ ${item.price}</p>
+</li>`;
+  })
+  productList.innerHTML = str; 
+}
